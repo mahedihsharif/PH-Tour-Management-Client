d75e84ecfa5be2cb429db2c2603eca269b909ce1
mahedihsharif <mahedi5061@gmail.com>
2025-08-15 21:58:44 +0600

    project-initialization

---
+++ src/pages/About.tsx
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div className="container py-16 px-4 mx-auto">
+      <h1>This is About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit
+        ducimus repudiandae eaque, fuga iusto qui aut nobis nihil corrupti unde
+        sequi dolorem ipsam eum id nostrum modi voluptas expedita quod?
+        Molestiae libero odit magnam quos dolorem eligendi nobis deserunt,
+        accusantium sed saepe expedita nisi corporis doloremque optio, laborum
+        perspiciatis vitae inventore placeat molestias accusamus esse quibusdam.
+        Ab quas nulla ad, maxime deleniti magnam? Quis ratione obcaecati est
+        commodi possimus deleniti labore aliquam nisi culpa velit perferendis
+        deserunt accusantium perspiciatis impedit quam aut in eveniet doloribus
+        laboriosam, debitis accusamus nesciunt dicta nam. Saepe reiciendis iste
+        non adipisci officiis quidem dolorem exercitationem!
+      </p>
+    </div>
+  );
+};
+
+export default About;
